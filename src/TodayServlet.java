a901167afa7e9a702065e930cf930234de0928de
LeeNaYoung <iirony@naver.com>
2018-05-12 21:21:55 +0900

    aboutme 프로젝트 완성

---
+++ src/TodayServlet.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodayServlet
+ */
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public TodayServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<h3 style='margin-left: 30px;'><a href='http://localhost:8080/aboutme/index.html'>메인화면</a></h3>");
+		
+		Date date=new Date();
+		SimpleDateFormat today = new SimpleDateFormat("yyyy/MM/dd hh:mm");
+
+		out.println("<h1 style='position: absolute;left: 50%;top: 50%;transform: translateX(-50%) translateY(-50%);'>현재시간: "+today.format(date)+"</h1>");
+	}
+
+}
